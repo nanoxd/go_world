cb338a65f2d82368d3250efb8affd2d2578c95ec
Fernando Paredes <nanoid.xd@gmail.com>
2013-07-04 10:01:36 -0700

    Test out basic arithmetic in Go

---
+++ numbers/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println("1 + 1 =", 1.0 + 1.0)
+  fmt.Println("15 / 3 =", 15 / 3)
+  fmt.Println("15 % 4 =", 15 % 4)
+  fmt.Println("32132 x 42452 =", 32132 * 42452)
+}
